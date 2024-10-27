4213ec11d51723e6738ac8252a63cceeebeada66
Azamat M <mukhamejanov.aza@gmail.com>
2024-10-27 23:03:23 +0500

    refactor: remove unsed .env variables

--- config/http.go
+++ config/http.go
@@ -3,17 +3,15 @@ package config
 import "os"
 
 type HTTPConfig struct {
-	Mode       string
-	Host       string
-	Port       string
-	ExposePort string
+	Mode string
+	Host string
+	Port string
 }
 
 func LoadHTTPConfig() *HTTPConfig {
 	return &HTTPConfig{
-		Mode:       os.Getenv("GIN_MODE"),
-		Host:       os.Getenv("HOST"),
-		Port:       os.Getenv("PORT"),
-		ExposePort: os.Getenv("EXPOSE_PORT"),
+		Mode: os.Getenv("GIN_MODE"),
+		Host: os.Getenv("HOST"),
+		Port: os.Getenv("PORT"),
 	}
 }
