c2de45e89229683343a2996ba3ed06936d9fbb29
Azamat M <mukhamejanov.aza@gmail.com>
2024-10-27 23:29:31 +0500

    chore: add GOPATH to setup

--- setup.sh
+++ setup.sh
@@ -3,6 +3,6 @@
 go install github.com/swaggo/swag/cmd/swag@v1.8.10
 go install github.com/pressly/goose/v3/cmd/goose@latest
 
-goose -dir "./db/migrations/" postgres "postgresql://$DB_USER:$DB_PASSWORD@$DB_HOST:$DB_PORT/$DB_NAME?search_path=$DB_SCHEMA" up
+$GOPATH/bin/goose -dir "./db/migrations/" postgres "postgresql://$DB_USER:$DB_PASSWORD@$DB_HOST:$DB_PORT/$DB_NAME?search_path=$DB_SCHEMA" up
 
-swag init -g cmd/main.go
+$GOPATH/bin/swag init -g cmd/main.go
